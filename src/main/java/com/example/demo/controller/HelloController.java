303e284df3d7f1cd5c37c345027262d5025711cf
Miguel Carneiro <miguel.carneiro@imanage.com>
2022-03-11 11:11:39 +0000

    Added logger

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+  Logger logger = LoggerFactory.getLogger(HelloController.class);
 
   @GetMapping("/")
   public String hello() {
-    System.out.println("IN");
+    logger.info("In");
     return "Hello world!";
   }
 }
